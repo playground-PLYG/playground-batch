570196a6b266a6c474a33d73d21ed7979fe82eec
hsm0711 <bingo8@nate.com>
2024-07-10 16:09:44 +0900

    배치 테스트

--- src/main/java/com/playground/batch/BatchApplication.java
+++ src/main/java/com/playground/batch/BatchApplication.java
@@ -1,11 +1,18 @@
 package com.playground.batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Import;
+import com.playground.batch.config.BatchConfig;
 
-@SpringBootApplication
+@EnableBatchProcessing(dataSourceRef = "batchDataSource", transactionManagerRef = "batchTransactionManager")
+@Import({BatchConfig.class})
 public class BatchApplication {
   public static void main(String[] args) {
-    SpringApplication.run(BatchApplication.class, args);
+    SpringApplication springApplication = new SpringApplicationBuilder(BatchApplication.class).web(WebApplicationType.NONE).build();
+
+    springApplication.run(args);
   }
 }
