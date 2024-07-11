69983bcc33d419e4a25c912caed6da2f4cf398d5
hsm0711 <bingo8@nate.com>
2024-07-11 17:44:25 +0900

    run.id 생성 로직 수정

--- src/main/java/com/playground/batch/config/CustomJobParametersIncrementer.java
+++ src/main/java/com/playground/batch/config/CustomJobParametersIncrementer.java
@@ -13,6 +13,8 @@ public class CustomJobParametersIncrementer implements JobParametersIncrementer 
 
   @Override
   public JobParameters getNext(JobParameters jobParameters) {
+    log.debug(">>> jobParameters : {}", jobParameters);
+    log.debug(">>> jobParameters : {}", jobParameters.getParameter("run.id") + "_" + format.format(new Date())));
     return new JobParametersBuilder().addString("run.id", jobParameters.getParameter("run.id") + "_" + format.format(new Date())).toJobParameters();
   }
 }
